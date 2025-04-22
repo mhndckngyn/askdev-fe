cf1d310782eb79c36dcaba937f7c75e7b00008a8
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2025-04-22 22:36:36 +0700

    feat: update UI trang Welcome
    Hiển thị tên website và slogan và một số icon trang trí
    Đưa "Quên mật khẩu" xuống dưới nút "Đăng nhập" (hỗ trợ bàn phím tốt hơn)

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,5 +1,13 @@
-import { Container, Paper, Tabs } from '@mantine/core';
-import { IconLogin, IconUserPlus } from '@tabler/icons-react';
+import { Container, Group, Paper, Tabs, Text, Title } from '@mantine/core';
+import {
+  IconBulb,
+  IconCode,
+  IconLogin,
+  IconMessageDots,
+  IconThumbUp,
+  IconUserPlus,
+  IconWorld,
+} from '@tabler/icons-react';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Login from './partials/Login';
@@ -8,33 +16,50 @@ import styles from './Welcome.module.css';
 import { useSearchParams } from 'react-router-dom';
 
 export default function Welcome() {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation('welcome');
+  const { t: tCommon } = useTranslation('common');
+
   const [searchParams] = useSearchParams();
   const initialTab = searchParams.get('tab') || 'login';
   const [activeTab, setActiveTab] = useState<string | null>(initialTab);
 
   return (
-    <Container size="xs" my={40}>
-      <Tabs value={activeTab} onChange={setActiveTab} variant="outline">
-        <Tabs.List grow>
-          <Tabs.Tab p="sm" value="login" leftSection={<IconLogin size={16} />}>
-            {t('login')}
-          </Tabs.Tab>
-          <Tabs.Tab value="signup" leftSection={<IconUserPlus size={16} />}>
-            {t('signup')}
-          </Tabs.Tab>
-        </Tabs.List>
+    <>
+      <div className={styles.header}>
+        <Title className={styles.title}>AskDev</Title>
+        <Text c="dimmed">{t('slogan')}</Text>
+        <Group gap="sm" className={styles.iconContainer}>
+          <IconCode color="var(--mantine-color-teal-5)" />
+          <IconMessageDots color="var(--mantine-color-pink-5)" />
+          <IconWorld color="var(--mantine-color-lime-5)" />
+          <IconThumbUp color="var(--mantine-color-blue-6)" />
+          <IconBulb color="var(--mantine-color-yellow-7)" />
+        </Group>
+      </div>
 
-        <Paper withBorder shadow="sm" p="xl" className={styles.paper}>
-          <Tabs.Panel value="login">
-            <Login />
-          </Tabs.Panel>
-
-          <Tabs.Panel value="signup">
-            <Signup />
-          </Tabs.Panel>
-        </Paper>
-      </Tabs>
-    </Container>
+      <Container size="xs" my={40}>
+        <Tabs value={activeTab} onChange={setActiveTab} variant="outline">
+          <Tabs.List grow>
+            <Tabs.Tab
+              p="sm"
+              value="login"
+              leftSection={<IconLogin size={16} />}>
+              {tCommon('login')}
+            </Tabs.Tab>
+            <Tabs.Tab value="signup" leftSection={<IconUserPlus size={16} />}>
+              {tCommon('signup')}
+            </Tabs.Tab>
+          </Tabs.List>
+          <Paper withBorder shadow="sm" p="xl" className={styles.paper}>
+            <Tabs.Panel value="login">
+              <Login />
+            </Tabs.Panel>
+            <Tabs.Panel value="signup">
+              <Signup />
+            </Tabs.Panel>
+          </Paper>
+        </Tabs>
+      </Container>
+    </>
   );
 }
--- src/types/Api.ts
+++ src/types/Api.ts
@@ -1,7 +1,7 @@
 // response body data
 export type ApiResponse<T = any> = {
   success: boolean;
-  message?: string;
+  message?: string; // quy ước: Nếu có lỗi và cần hiển thị cho người dùng, set ở đây
   content: T;
   error?: string;
   statusCode: number;
