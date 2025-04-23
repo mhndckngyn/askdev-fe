5e68db69f688616de679287d79a376349b7a0739
ASUS PC <22520927@gm.uit.edu.vn>
2025-04-23 23:04:39 +0700

    feat: Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
+import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
-import '@mantine/tiptap/styles.css';
 import '@mantine/dropzone/styles.css';
-import { Loader, MantineProvider } from '@mantine/core';
+import '@mantine/tiptap/styles.css';
+import { Fragment, useEffect } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import styles from './App.module.css';
-import Homepage from './pages/Homepage';
-import Welcome from './pages/Welcome';
-import NavBar from './layouts/NavBar';
-import Footer from './layouts/Footer';
-import NotFound from './layouts/NotFound';
-import AccountSuspended from './layouts/AccountSuspended';
-import TagsPage from './pages/TagsPage';
-import PostQuestion from './pages/PostQuestion';
+import ActionModal from './components/ActionModal';
 import ErrorModal from './components/ErrorModal';
+import AccountSuspended from './layouts/AccountSuspended';
+import Footer from './layouts/Footer';
+import NavBar from './layouts/NavBar';
+import NotFound from './layouts/NotFound';
+import ChangePassword from './pages/ChangePassword';
+import EditProfile from './pages/EditProfile';
+import EmailVerify from './pages/EmailVerify';
+import Homepage from './pages/Homepage';
+import PostQuestion from './pages/PostQuestion';
 import ProfilePage from './pages/ProfilePage';
 import ProfileRedirect from './pages/ProfileRedirect/ProfileRedirect';
-import EditProfile from './pages/EditProfile';
-import ChangePassword from './pages/ChangePassword';
-import { ActionModal } from './components/ActionModal/ActionModal';
+import TagsPage from './pages/TagsPage';
+import Welcome from './pages/Welcome';
 import { useUserStore } from './stores/useUserStore';
-import { Fragment, useEffect } from 'react';
-import EmailVerify from './pages/EmailVerify';
-import Questions from './pages/Question/Detail';
+import QuestionsDetail from './pages/Question/Detail';
 
 export default function App() {
-  const { fetchUser, loading } = useUserStore();
+  const { fetchUser } = useUserStore();
 
   useEffect(() => {
     fetchUser();
@@ -34,32 +34,28 @@ export default function App() {
     <Router>
       <MantineProvider>
         <div className={styles.app}>
-          {loading ? (
-            <Loader />
-          ) : (
-            <Fragment>
-              <NavBar />
-              <div className={styles.content}>
-                <Routes>
-                  <Route path="/welcome" element={<Welcome />} />
-                  <Route path="/questions/detail" element={<Questions />} />
-                  <Route path="/" element={<Homepage />} />
-                  <Route path="/tags" element={<TagsPage />} />
-                  <Route path="/questions/post" element={<PostQuestion />} />
-                  <Route path="/profile" element={<ProfileRedirect />} />
-                  <Route path="/profile/:id" element={<ProfilePage />} />
-                  <Route path="/profile/edit" element={<EditProfile />} />
-                  <Route
-                    path="/change-password"
-                    element={<ChangePassword />}></Route>
-                  <Route path="/verify-email" element={<EmailVerify />} />
-                  <Route path="/suspended" element={<AccountSuspended />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </div>
-              <Footer />
-            </Fragment>
-          )}
+          <Fragment>
+            <NavBar />
+            <div className={styles.content}>
+              <Routes>
+                <Route path="/welcome" element={<Welcome />} />
+                <Route path="/" element={<Homepage />} />
+                <Route path="/tags" element={<TagsPage />} />
+                <Route path="/questions/detail" element={<QuestionsDetail />} />
+                <Route path="/questions/post" element={<PostQuestion />} />
+                <Route path="/profile" element={<ProfileRedirect />} />
+                <Route path="/profile/:id" element={<ProfilePage />} />
+                <Route path="/profile/edit" element={<EditProfile />} />
+                <Route
+                  path="/change-password"
+                  element={<ChangePassword />}></Route>
+                <Route path="/verify-email" element={<EmailVerify />} />
+                <Route path="/suspended" element={<AccountSuspended />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+            <Footer />
+          </Fragment>
           <ActionModal />
           <ErrorModal />
         </div>
